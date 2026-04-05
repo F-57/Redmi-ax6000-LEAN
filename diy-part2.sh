f08527f66c91537413ebfa1676c8631d187d5a07
楠方 <80924478+F-57@users.noreply.github.com>
2026-04-06 00:23:37 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,8 +10,15 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-# Custom for REDMI AX6000
-sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_generate
+
+# 1. 替换 LAN 口默认 IP (192.168.1.1 -> 10.0.0.1)
+sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
+
+# 2. 替换其他接口的递增网段 (192.168.$((...)) -> 10.0.$((...)))
+sed -i 's/192.168.\$((addr_offset++))/10.0.\$((addr_offset++))/g' package/base-files/files/bin/config_generate
+
+# 512布局
+sed -i 's/reg = <0x600000 0x6e00000>/reg = <0x600000 0x1ea00000>/' target/linux/mediatek/dts/mt7986a-xiaomi-redmi-router-ax6000.dts
 
 #sed -i 's/zh_cn/auto/g' feeds/luci/modules/luci-base/root/etc/uci-defaults/luci-base 
 #sed -i '/AUTOLOAD:=$(call AutoProbe,mt7915e)/a \  MODPARAMS.mt7915e:=wed_enable=Y' package/kernel/mt76/Makefile
